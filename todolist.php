92bbc948e7827937650acaba0335c0015fad9021
felix-hauger <felix.hauger@laplateforme.io>
2023-02-23 14:39:20 +0100

    todolist.php - added basic html structure

---
+++ todolist.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vos tâches | To Do List</title>
+</head>
+<body>
+    <h1>Vos tâches</h1>
+    <form method="post">
+        <input type="text" name="" id="" placeholder="Votre tâche">
+        <button id="add" name="add">Ajouter une tâche</button>
+    </form>
+    <h2>Vos tâches à finir</h2>
+    <hr>
+    <h2>Vos tâches achevées</h2>
+</body>
+</html>
