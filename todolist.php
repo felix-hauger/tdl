2bc834f6ae7e742c992cdeffc7c5017161b01620
felix-hauger <felix.hauger@laplateforme.io>
2023-02-23 15:41:59 +0100

    todolist.php - added php code to create task in db

--- todolist.php
+++ todolist.php
@@ -1,5 +1,26 @@
 <?php
 
+if (isset($_POST['add'])) {
+    $content = $_POST['content'];
+    $user_id = 1;
+    $date = new DateTime();
+    $creation_date = $date->format('Y-m-d H:i:s');
+
+    $pdo = new PDO('mysql:dbname=todolist;host=localhost;charset=utf8mb4', 'root', '');
+
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'INSERT INTO tasks (content, creation_date, user_id) VALUES (:content, :creation_date, :user_id)';
+
+    $insert = $pdo->prepare($sql);
+
+    $insert->bindParam(':content', $content);
+    $insert->bindParam(':creation_date', $creation_date);
+    $insert->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+
+    $insert->execute();
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +36,17 @@
 <body>
     <h1>Vos tâches</h1>
     <form method="post">
-        <input type="text" name="" id="" placeholder="Votre tâche">
-        <button id="add" name="add">Ajouter une tâche</button>
+        <input type="text" name="content" id="content" placeholder="Votre tâche">
+        <button type="submit" id="add" name="add">Ajouter une tâche</button>
     </form>
-    <h2>Vos tâches à finir</h2>
+    <section id="tofinish">
+        <h2>Vos tâches à finir</h2>
+        <div class="task-container"></div>
+    </section>
     <hr>
-    <h2>Vos tâches achevées</h2>
+    <section id="achieved">
+        <h2>Vos tâches achevées</h2>
+        <div class="task-container"></div>
+    </section>
 </body>
 </html>
