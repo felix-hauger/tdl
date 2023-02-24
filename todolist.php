09111e67cc733a88baef68829083c699f0c299d7
felix-hauger <felix.hauger@laplateforme.io>
2023-02-24 14:07:01 +0100

    todolist.php - use getAllTasks() function to get all tasks from db + use DbConnection singleton instance

--- todolist.php
+++ todolist.php
@@ -1,9 +1,38 @@
 <?php
 
-$pdo = new PDO('mysql:dbname=todolist;host=localhost;charset=utf8mb4', 'root', '');
+require_once 'class/DbConnection.php';
 
+$pdo = DbConnection::getDb();
 
-function addTask($content, $db) {
+function getAllTasks(PDO $db) {
+    $sql = 'SELECT * FROM tasks WHERE user_id = :user_id';
+    $select = $db->prepare($sql);
+    $user_id = 1;
+
+    $select->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+
+    if ($select->execute()) {
+        echo 'ok';
+        $tasks = $select->fetchAll(PDO::FETCH_ASSOC);
+
+        var_dump($tasks);
+        // $user_tasks = [
+        //     'created' => [],
+        //     'completed' => []
+        // ];
+        $created_tasks = [];
+        $completed_tasks = [];
+        foreach ($tasks as $task) {
+            if ($task['completion_date'] === null) {
+                $created_tasks[] = $task;
+            } else {
+                $completed_tasks[] = $task;
+            }
+        }
+    }
+}
+
+function addTask($content, PDO $db) {
     $user_id = 1;
     $date = new DateTime();
     $creation_date = $date->format('Y-m-d H:i:s');
@@ -47,12 +76,16 @@ if (isset($_POST['add'])) {
     </form>
     <section id="tofinish">
         <h2>Vos tâches à finir</h2>
-        <div class="task-container"></div>
+        <div class="task-container">
+            <?php getAllTasks($pdo) ?>
+        </div>
     </section>
     <hr>
     <section id="achieved">
         <h2>Vos tâches achevées</h2>
-        <div class="task-container"></div>
+        <div class="task-container">
+
+        </div>
     </section>
 </body>
 </html>
