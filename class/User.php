e5f716c705fca5fb28f613655bd8e863cbf85546
felix-hauger <felix.hauger@laplateforme.io>
2023-02-27 18:15:01 +0100

    class/User.php - added isLoginInDb() method to check if email exists in db

--- class/User.php
+++ class/User.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'DbConnection.php';
 class User
 {
     /**
@@ -26,6 +26,63 @@ class User
     {
         
     }
+
+    public function isLoginInDb($email): bool
+    {
+        $sql = 'SELECT COUNT(id) FROM users WHERE email = :email';
+
+        $select = DbConnection::getDb()->prepare($sql);
+
+        $select->bindParam(':email', $email);
+
+        $select->execute();
+
+        $user = $select->fetch(PDO::FETCH_NUM);
+
+        return $user[0];
+    }
+
+    public function register($login, $password, $email, $firstname, $lastname)
+    {
+        // Assign to $email false or $email filtered by filter_var, & throw error if false at the same time
+        if (!$email = filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('Format email invalide');
+        } else {
+            $search_logins = $this->verifyLogins($login, $email);
+
+            if ($search_logins['found_login']) {
+                throw new Exception('Le login existe est déjà utilisé');
+            } elseif ($search_logins['found_email']) {
+                throw new Exception('Adresse mail déjà utilisée');
+            } else {
+                $hashed_password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
+
+                $sql = 'INSERT INTO users (login, password, email, firstname, lastname) VALUES (:login, :password, :email, :firstname, :lastname)';
+
+                $insert = DbConnection::getDb()->prepare($sql);
+
+                $insert->bindParam(':login', $login);
+                $insert->bindParam(':password', $hashed_password);
+                $insert->bindParam(':email', $email);
+                $insert->bindParam(':firstname', $firstname);
+                $insert->bindParam(':lastname', $lastname);
+
+                if ($insert->execute()) {
+                    return true;
+                } else {
+                    throw new Exception('Échec lors de l\'inscription');
+                }
+
+            }
+        }
+    }
+
+
+    // public function register($email, $password, $firstname, $lastname)
+    // {
+    //     $sql = 'INSERT INTO users (email, password, firstname)';
+    // }
+
 
     /**
      * Get the value of id
@@ -99,3 +156,8 @@ class User
         return $this;
     }
 }
+
+$user = new User();
+$test = $user->isLoginInDb('admin@admin.com');
+
+var_dump($test);
