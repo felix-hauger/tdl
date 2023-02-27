6abc4c6ebedae9c9d88a8ca1bf3b12d23ec23b82
felix-hauger <felix.hauger@laplateforme.io>
2023-02-27 02:05:12 +0100

    js/script.js - display creation or completion dates for tasks

--- js/script.js
+++ js/script.js
@@ -10,25 +10,33 @@ function getTasks() {
                 let container;
                 const taskBox = document.createElement('div'),
                     deleteBtn = document.createElement('button'),
-                    content = document.createElement('p');
+                    content = document.createElement('p'),
+                    displayedDate = document.createElement('span');
 
                 content.innerHTML = task.content;
                 deleteBtn.innerHTML = 'Supprimer';
-                // deleteBtn.id = 'task' . task.id;
-                deleteBtn.setAttribute('id', 'task-' . task.id)
+                console.log(task);
+                deleteBtn.setAttribute('id', 'task-' + task.id);
 
                 taskBox.append(content);
 
                 if (task.completion_date === null) {
-                    console.log(task)
                     // code pour tâche inachevées
+                    // console.log(task)
                     const finishBtn = document.createElement('button');
-                    finishBtn.innerHTML = 'Terminer';
                     container = document.querySelector('#tofinish .task-container');
+
+                    displayedDate.innerHTML = 'ajouté le ' + task.creation_date;
+                    finishBtn.innerHTML = 'Terminer';
+
+                    taskBox.append(displayedDate);
                     taskBox.append(finishBtn);
                 } else {
                     // code pour tâches achevées
+                    displayedDate.innerHTML = 'terminé le ' + task.completion_date;
                     container = document.querySelector('#achieved .task-container');
+
+                    taskBox.append(displayedDate);
                 }
 
                 taskBox.append(deleteBtn);
