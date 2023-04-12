a9c82a7dc06935a6d306d11804989d69997f24a3
felix-hauger <felix.hauger@laplateforme.io>
2023-04-12 18:24:16 +0200

    register.php - added code to register with try..catch

--- register.php
+++ register.php
@@ -1,7 +1,36 @@
+<?php
+if (isset($_POST['submit'])) {
+    require_once 'class/User.php';
+
+    $email = htmlspecialchars(trim($_POST['email'], ENT_QUOTES));
+    $password = htmlspecialchars(trim($_POST['password'], ENT_QUOTES));
+    $confirm = htmlspecialchars(trim($_POST['confirm-password'], ENT_QUOTES));
+    $firstname = htmlspecialchars(trim($_POST['firstname'], ENT_QUOTES));
+    $lastname = htmlspecialchars(trim($_POST['lastname'], ENT_QUOTES));
+
+    $register_user = new User();
+
+    try {
+        if ($register_user->register($email, $password, $firstname, $lastname)) {
+            $register_success = 'Inscription réussie !';
+        }
+    } catch (Exception $e) {
+        $register_error = $e->getMessage();
+    }
+}
+
+
+?>
 <form action="" method="post">
     <input type="email" name="email" id="email" placeholder="Email">
     <input type="password" name="password" id="password" placeholder="Mot de Passe">
+    <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirmer Mot de Passe">
     <input type="text" name="firstname" id="firstname" placeholder="Prénom">
     <input type="text" name="lastname" id="lastname" placeholder="Nom de Famille">
-    <input type="submit" name="submit" value="Connexion">
+    <input type="submit" name="submit" value="Inscription">
+    <?php if (isset($register_error)): ?>
+        <p><?= $register_error ?></p>
+    <?php elseif (isset($register_success)): ?>
+        <p><?= $register_success ?></p>
+    <?php endif ?>
 </form>
